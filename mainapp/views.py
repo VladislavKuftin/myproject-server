daf71246fd069ada262f5f0eceebb687c998304b
Kuftin Vladislav <boer5678@gmail.com>
2021-11-25 18:43:52 +0300

    for lesson2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
-    return render(request, "mainapp/index.html")
+    menu = {'title':'Магазин'}
+    return render(request, "mainapp/index.html", menu)
+
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    menu = {'title': "Продукты"}
+    return render(request, "mainapp/products.html",menu)
+
 
 def contact(request):
-    return render(request, "mainapp/contact.html")
+    menu = {'title': "Контакты"}
+    return render(request, "mainapp/contact.html", menu)
+
+
+def context(request):
+    context = {
+        'title':'test context',
+        'header':'Добро пожаловать на сайт',
+        'username':'John',
+        'products': [
+            {'name':'Стулья', 'price':5678},
+            {'name':'Диваны', 'price':15678},
+            {'name':'Столы', 'price':25678},
+        ]
+    }
+    return render(request, "mainapp/text_context.html", context)
+
+def menu(requset):
+    links_menu = [
+        {'href':'products_all', 'name':'все'},
+        {'href':'products_home', 'name':'дом'},
+        {'href':'products_office', 'name':'офис'},
+        {'href':'products_classic', 'name':'классика'},
+    ]
+
+    return render(requset, 'inc_categories_menu.html', links_menu)
