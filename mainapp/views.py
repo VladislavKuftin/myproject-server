b0384e99d6ef17b309174bb61065340b72c3fe08
Kuftin Vladislav <boer5678@gmail.com>
2021-12-13 18:47:09 +0300

    Hw 5 done!

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,7 +10,17 @@ from mainapp.models import Product, ProductCategory
 def get_basket(user):
     if user.is_authenticated:
         return Basket.objects.filter(user=user)
-    return []
+    return None
+
+
+def get_hot_product():
+    return random.sample(list(Product.objects.all()), 1)[0]
+
+
+def get_same_products(hot_product):
+    products_list = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return products_list
+
 
 def index(request):
     context = {
@@ -19,6 +29,7 @@ def index(request):
         'basket': get_basket(request.user)
     }
     return render(request, "mainapp/index.html", context)
+
 
 def contact(request):
     context = {
@@ -49,8 +60,8 @@ def products(request, pk=None):
         }
         return render(request, "mainapp/products_list.html", context=context)
 
-    hot_product = random.sample(list(Product.objects.all()), 1)[0]
-    same_products = Product.objects.all()[3:5]
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
     context = {
         'links_menu': links_menu,
         'title': 'Продукты',
@@ -59,3 +70,12 @@ def products(request, pk=None):
         'basket': get_basket(request.user)
     }
     return render(request, "mainapp/products.html", context=context)
+
+def product(requset, pk):
+    links_menu = ProductCategory.objects.all()
+    context = {
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(requset.user),
+        'links_menu': links_menu
+    }
+    return render(requset, 'mainapp/product.html', context)
